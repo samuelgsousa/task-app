fc631ff2249e25e30079cacb0e0a0960c091b692
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-20 14:14:16 -0300

    Revert "alterando rota para deletar"
    This reverts commit 6ce558bc915a0685f1613183c02d45b6b1836ccb.

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 });
 
 // Rota para deletar uma tarefa
-app.delete('/', async (req, res) => {
+app.delete('/:id', async (req, res) => {
   const { id } = req.params;
   console.log('parametros em taks: ' + `ObjectId('${id}')`)
   try {
