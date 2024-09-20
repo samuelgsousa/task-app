f125bbfb1174e37bc00559a2b662b0540a307ffa
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-20 15:07:16 -0300

    Revert "Update Task.js"
    This reverts commit b5056424e94ac48bf62c2d454df4ed4ebffd41c2.

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  id: {type: Number, unique: true, required: true},
   text: { type: String, required: true },
   completed: { type: Boolean, default: false },
 });
