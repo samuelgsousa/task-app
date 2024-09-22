dfd6279fa4f93d03a8ac25e190c2de2790f275b6
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-21 21:53:41 -0300

    Update Todo.js

--- src/app/components/Todo.js
+++ src/app/components/Todo.js
@@ -1,5 +1,5 @@
 "use client"; 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import { useRouter } from 'next/navigation';
 import './Todo.scss';
 
