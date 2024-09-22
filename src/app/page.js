3d132cdfd47ea1db70f04332417f3ff3650ee418
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-09-21 22:17:48 -0300

    Revert "Mais uma vez deu errado"
    This reverts commit 323e7cfdaabba7a5bd5e15119d5376462c925b7b.

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,17 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Login from "./components/Login";
 
-export default function Home({ setShowWelcomeMessage }) {
+export default function Home() {
 
   return (
     <div>
-      <Login setShowWelcomeMessage={setShowWelcomeMessage} ></Login>
+      <Login></Login>
 
       <div className="aware">
         <h1>Usuário: Marcus</h1>
         <h1>Senha: admin123</h1>
 
-        <p>Obs: A fim de tornar a experiência mais completa e funcional, adicionei um sistema de autenticação e um banco de dados remoto. Assim os dados ficam armazenados e é possível acessar a mesma lista de tarefas em qualquer dispositivo</p>
+        <p>Obs: a fim de tornar a experiência mais completa e funcional, adicionei um sistema de autenticação e um banco de dados remoto. Assim os dados ficam armazenados e é possível acessar a mesma lista de tarefas em qualquer dispositivo</p>
       </div>
     </div>
     
